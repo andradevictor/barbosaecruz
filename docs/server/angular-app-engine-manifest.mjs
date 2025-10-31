3cc0a4603cd88c039ac5886122051ac6fd55b617
Victor A. Barbosa <victor@wetune.com.br>
2025-10-31 15:05:59 -0300

    .

--- docs/server/angular-app-engine-manifest.mjs
+++ docs/server/angular-app-engine-manifest.mjs
@@ -1,6 +1,6 @@
 
 export default {
-  basePath: '/inggatec/barbosaecruz',
+  basePath: '/barbosaecruz',
   supportedLocales: {
   "en-US": ""
 },
--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -2,19 +2,19 @@
 export default {
   bootstrap: () => import('./main.server.mjs').then(m => m.default),
   inlineCriticalCss: true,
-  baseHref: '/inggatec/barbosaecruz',
+  baseHref: '/barbosaecruz/',
   locale: undefined,
   routes: [
   {
     "renderMode": 2,
-    "route": "/inggatec/barbosaecruz"
+    "route": "/barbosaecruz"
   }
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 28002, hash: '2e3df65107f3b0b024b3115195d22e80a6553e3441411454ef7f654a59aa8364', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 17161, hash: '927cfcdb80b90a2ff045f8cebae2ed1cdff83ce5f9b3ad49132fe1856210328f', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 65462, hash: '1d8e80f7d7f784dbd469a5600a504373ec080976ed789ab29b4939e1db9d60dd', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 27994, hash: 'bee1dcbe22114ce7c23d77f6f315200e8dfb69a1769c233b09934dc28568bb9e', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 17153, hash: '9e98f71596434841de1053dda1dd144e04cb1b50a8788e671f25398aed436c25', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 65454, hash: '49ea047bb91f9458bea9602ab8c7cf384ede402b2af572ac8c29bf8217f855c4', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-OBQTMA3M.css': {size: 237944, hash: 'LnfsZC6fqY0', text: () => import('./assets-chunks/styles-OBQTMA3M_css.mjs').then(m => m.default)}
   },
 };
